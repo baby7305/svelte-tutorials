0b0b1a5558bbc71317ab98bf0f08399f4a9970d9
baby7305 <songhaibei7305@163.com>
2019-06-17 13:40:10 +0800

    引入简单交互样式

--- src/App.svelte
+++ src/App.svelte
@@ -1,1 +1,15 @@
-<h1>Hello baby!</h1>
+<script>
+	let numbers = [1, 2, 3, 4];
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+	}
+
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+</script>
+
+<p>{numbers.join(' + ')} = {sum}</p>
+
+<button on:click={addNumber}>
+	Add a number
+</button>
