19fcea8be1a3ee661543957aac0687795e09d48d
baby7305 <songhaibei7305@163.com>
2019-06-17 11:06:37 +0800

    引入点击事件

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,10 @@
 	let count = 0;
 
 	function handleClick() {
-		// event handler code goes here
+		count += 1;
 	}
 </script>
 
-<button>
+<button on:click={handleClick}>
 	Clicked {count} {count === 1 ? 'time' : 'times'}
 </button>
